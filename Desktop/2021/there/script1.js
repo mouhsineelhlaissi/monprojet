e25d918bfac9a9e0f5d5e9e485d661350ba0ea7b
mouhsineelhlaissi <mouhsinesm@gmail.com>
2021-05-19 14:18:40 +0200

    had l9lawi 3assbni

--- Desktop/2021/there/script1.js
+++ Desktop/2021/there/script1.js
@@ -1,17 +1,6 @@
-var menuItem=document.getElementById("menu").getElementsByTagName("li")
-var btn=document.getElementById("bt")
-var headline=document.getElementById("demo");
-var myMenu=document.getElementById("menu")
-var counter=1;
-menuItem[0].innerHTML = "hello world"
-for(i=0;i<3;i++){
-    menuItem[i].addEventListener("click",selectItem);
-}
-function selectItem(){
-   headline.innerHTML=this.innerHTML;
-   
-}
-btn.addEventListener("click",newItem);
-function newItem(){
-    myMenu.innerHTML+="<li> new line" +counter+ "</li>",counter++;
+var leiba=document.getElementById("twichia")
+var menu=document.getElementById("bt");
+menu.addEventListener("click",fct);
+ function fct (){
+    console.log(leiba);
 }
